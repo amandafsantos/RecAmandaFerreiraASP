cb44b774ad4d4ba69736f1d31b8569c89be81da2
gabrielhnrq <gabriel.hnrq@gmail.com>
2018-05-06 22:03:43 -0300

    Alteração

---
+++ RecAmandaFerreiraASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecAmandaFerreiraASP.MvcApplication" Language="C#" %>
---
+++ RecAmandaFerreiraASP/Models/DataAccessObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RecAmandaFerreiraASP.Models
+{/// <summary>
+/// Chamada do Banco Modelo para Tratar (DAO)
+/// </summary>
+    public class DataAccessObject:DbContext
+    {
+        public DataAccessObject() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<RecAmandaFerreiraASP.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<RecAmandaFerreiraASP.Models.News> News { get; set; }
+
+    }
+}
